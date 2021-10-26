f9afc0de047513944b4b753c2a2fa52c7893f04c
nospoonvue <nospoonvue@nospoon.nl>
2021-10-26 13:06:21 +0100

    esf

---
+++ src/views/Teacher copy.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+							
+<DynamicWebText :label="'Teachers manual'" v-bind:language="this.$store.state.language" :key="getKey+this.$store.state.language"/>
+
+  </div>
+</template>
+
+<script>
+import DynamicWebText from '@/components/DynamicWebText.vue'
+
+
+
+export default 
+{
+    name: "teachers",
+    components: {DynamicWebText},
+
+    data() 
+    {
+    return {
+        keyId:1   
+        }
+    },
+    computed: {
+        getKey: function () {
+            
+            return this.keyId++;
+        }
+  },
+    created() 
+    {
+       
+    },
+}
+
+
+</script>
