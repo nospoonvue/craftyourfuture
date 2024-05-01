d9c89e621ca5d0ae52551461beb240fe7354e6ed
nospoonvue <46303745+nospoonvue@users.noreply.github.com>
2024-05-02 00:03:50 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,7 +10,8 @@ Vue.config.productionTip = false
 //Vue.prototype.$baseUrl = "https://script.google.com/macros/s/AKfycbzFu9XjeWN1BKMEYe6kNv16jQhiatpqBTFj0mlEfg/exec"
 //Vue.prototype.$baseUrl = "https://script.google.com/macros/s/AKfycbwML4WonnuCnyHdCw2egjIBnvllhJH9Sb4PLapuGe73qwHcv6MQ4etu8g/exec"
 //Vue.prototype.$baseUrl = "https://script.google.com/macros/s/AKfycbx5GLrXhpdBh2avqiQLvHYSs5TmPTPbK_tAGu5zeUoL2GdJ3hKbsKLs/exec"
-Vue.prototype.$baseUrl = "https://script.google.com/macros/s/AKfycbx3nMtET_Xl9svvdu3Q8_1qudPss8ryRYknx-wQo4EK2mHwjWDDn9eOQuerH_iheGjkgQ/exec"
+			    
+Vue.prototype.$baseUrl = "https://script.google.com/macros/s/AKfycbxLSHbMwMQo_pEgUjNRE16VlGarbv0KLpgi0go_WqVmTQUpskgH9s_HrQHCZqpUDOOw/exec"
 Vue.prototype.$appName = "CraftYourFuture"
 
 
